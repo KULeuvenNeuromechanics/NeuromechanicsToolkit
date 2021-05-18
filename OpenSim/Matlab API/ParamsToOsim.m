ce07df9f189d171e65ed8d929fa249bc1018ced9
VDB-Bram <bramvandenbosch@outlook.com>
2021-05-18 10:06:21 +0200

    Update ParamsToOsim.m

--- OpenSim/Matlab API/ParamsToOsim.m
+++ OpenSim/Matlab API/ParamsToOsim.m
@@ -5,12 +5,11 @@ function ParamsToOsim(muscleParams,muscleNames,modelPath)
 %
 % INPUT:
 %   -muscleParams-
-%   * 5 x nMuscles structure with
+%   * 4 x nMuscles structure with
 %       * FMo         : maximal isometric force
 %       * lMo         : optimal fiber length
 %       * lTs         : tendon slack length 
-%       * alphao      : optimal pennation angle OR penation angle at lMo
-%       * kT          : tendon stiffness; this parameter is not used.
+%       * alphao      : penation angle at lMo
 %  
 %   -muscleNames-  
 %   * cell structure with the muscles from which muscle parameters are 
@@ -28,34 +27,29 @@ function ParamsToOsim(muscleParams,muscleNames,modelPath)
 % Original date: 12/05/2021
 
 
-import org.opensim.modeling.*
+import org.opensim.modeling.* % import OpenSim libraries
 
 FMo    = muscleParams.FMo;
 lMo    = muscleParams.lMo;
 lTs    = muscleParams.lTs;
 alphao = muscleParams.alphao;
-kT     = muscleParams.kT;
 
 listing = dir(modelPath);
 modelFilePath = listing.folder;
 modelFile     = listing.name;
-newModelFile = 'newParams.osim'; % name of the new osim model 
+newModelFile = 'newParams.osim'; % name of the new osim model, you can change this as you like 
 
-% Load the original model and initialize
+% load the original model and initialize
 model = Model(fullfile(modelFilePath, modelFile));
 model.initSystem();
 
-% Get the muscles of the original model
-muscles = model.getMuscles(); 
 
-% Count the muscles with new parameters
-nMuscles = length(muscleNames);
-
+muscles  = model.getMuscles(); % get the muscles of the original model
+nMuscles = length(muscleNames); % count the muscles with new parameters
 disp(['Number of muscles with new parameters: ' num2str(nMuscles)]);
 
-% loop through muscles to adapt new parameters
+% loop through muscles with new parameters
 for i = 1:nMuscles 
-    
     % get the muscle in the generic model
     currentMuscle = muscles.get(muscleNames{i});
 
@@ -63,10 +57,7 @@ for i = 1:nMuscles
     currentMuscle.setMaxIsometricForce(FMo(i));
     currentMuscle.setOptimalFiberLength(lMo(i));
     currentMuscle.setTendonSlackLength(lTs(i));
-    currentMuscle.setPennationAngleAtOptimalFiberLength(alphao(i));
-    
-    % er is geen mogelijkheid om kT te definiëren in het model? 
-    
+    currentMuscle.setPennationAngleAtOptimalFiberLength(alphao(i));    
 end
  
 % save the new model to an osim file
