6158c8239c32fc81a4341852f135c1478f1b206a
VDB-Bram <bramvandenbosch@outlook.com>
2021-05-19 14:24:30 +0200

    Create FunctionTemplate.m

---
+++ FunctionTemplate.m
@@ -0,0 +1,31 @@
+function [outputArg1,outputArg2] = FunctionName(inputArg1,inputArg2)
+% --------------------------------------------------------------------------
+%FUNCTIONNAME 
+%   Detailed explanation goes here
+% 
+% INPUT:
+%   -inputArg1-
+%   * description of this input
+%  
+%   -inputArg2-
+%   * description of this input
+% 
+% OUTPUT:
+%   -outputArg1-
+%   * description of this output
+%
+%   -inputArg1-
+%   * description of this output
+% 
+% Original author: FULL NAME
+% Original date: DD/MM/YYYY
+%
+% Last edit by: FULL NAME
+% Last edit date: DD/MM/YYYY
+% --------------------------------------------------------------------------
+
+
+
+
+end
+
