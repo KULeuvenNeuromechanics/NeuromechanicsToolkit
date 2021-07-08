c7d68c52a9c39988bcaa0f10a6ee46f2f7a930b7
VDB-Bram <bramvandenbosch@outlook.com>
2021-07-08 10:44:46 +0200

    Update SelectRotationMatrix.m
    added the rotation matrix for gait lab 1 in Pellenberg

--- Dataprocessing/SelectRotationMatrix.m
+++ Dataprocessing/SelectRotationMatrix.m
@@ -2,6 +2,7 @@ function rot = SelectRotationMatrix(labname)
 % SelectRotationMatrix outputs a struct based on the lab that is given as
 % an input.
 % 
+<<<<<<< Updated upstream
 % Inputs: 
 % labname = name of the lab the data is recorded. This can be 
 % "overground_Mall" for the overground walkway in the MALL, 
@@ -13,6 +14,26 @@ function rot = SelectRotationMatrix(labname)
 % 
 % If transformation for your lab are missing. Please ad an elseif branch
 % with the correct rotatations.
+=======
+% INPUT:
+%   - labname -
+%	* name of the lab the data is recorded. This can be 
+%		*	"overground_MALL" for the overground walkway in the MALL
+%		*	"treadmill_MALL" for the treadmill in the MALL
+%		*	"CMAL_1" for gait lab 1 in Pellenberg
+%  
+% OUTPUT:
+%   - rot -
+%	* rot is a struct containing the field Markers and ForcePlate which 
+%	are the rotation matrices for the respective data.
+%
+% Original author: Wouter Muijres
+% Original date: 01/06/2021
+%
+% Last edit by: Bram Van Den Bosch
+% Last edit date: 08/07/2021
+% --------------------------------------------------------------------------
+>>>>>>> Stashed changes
 
 rot = struct();
 if strcmp(labname,'overground_MALL')
@@ -20,6 +41,9 @@ if strcmp(labname,'overground_MALL')
     rot.ForcePlate  = [0, 1, 0;1, 0, 0;0, 0, -1];
 elseif strcmp(labname,'treadmill_MALL')
     rot.Markers     = [0, -1, 0;0, 0, 1;-1, 0, 0];
+    rot.ForcePlate  = [-1, 0, 0;0, 1, 0;0, 0, -1];
+elseif strcmp(labname,'CMAL_1')
+    rot.Markers     = [-1, 0, 0;0, 0, 1;0, 1, 0];
     rot.ForcePlate  = [-1, 0, 0;0, 1, 0;0, 0, -1];
 end
 
