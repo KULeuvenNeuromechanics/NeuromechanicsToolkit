b8f807b12859d6eb5b5fa5ddffd4a1c7e3bfcdc7
MaartenAfschrift <maarten.afschrift@kuleuven.be>
2021-08-24 20:41:44 +0200

    Update OpenSimAPI

---
+++ OpenSimAPI/Opensim_IK.m
@@ -0,0 +1,86 @@
+function  []=Opensim_IK( model_dir,input_file,output_settings,output_IK,event,generic_settings_IK,varargin)
+% Opensim_IK Uses API to solve inverse dynamics
+%
+%   INPUT:
+%   (1) model_dir=             full filename opsim model 
+%   (2) input_file=            full filename file with marker cordinates (*.trc)
+%   (3) output_settings=       path+name to save the output
+%   (4) output_IK=             path+name of the IK output file
+%   (5) event=                 start and endpoin time (in seconds)
+%   (6) generic_settings_IK=   path+name generic IK settings file (.xml file)
+%   (7) varargin:
+%       (1) Name of diary:
+%
+%   OUTPUT:
+%
+%   DEPENDENCIES:
+%   (1) you have to install the opensim API to run this function (tested with 4.2)
+%
+%   AUTHOR:
+%   Maarten Afschrift
+
+% variable input arguments: diary
+BoolDiary = 0;
+if ~isempty(varargin)
+    BoolDiary = 1;
+    NameDiary = varargin{1};
+end
+
+% test if we have to create the output folders
+[OutPath,~,~] = fileparts(output_settings);
+if ~isfolder(OutPath)
+    mkdir(OutPath);
+end
+
+[OutPath,~,~] = fileparts(output_IK);
+if ~isfolder(OutPath)
+    mkdir(OutPath);
+end
+
+if BoolDiary
+    [OutPath,~,~] = fileparts(NameDiary);
+    if ~isfolder(OutPath)
+        mkdir(OutPath);
+    end
+    diary(NameDiary)
+end
+
+% load the Opensim classes
+import org.opensim.modeling.*
+
+% set up the IK tool
+ikTool = InverseKinematicsTool(generic_settings_IK);
+
+% use the loaded model
+model=Model(model_dir);
+model.initSystem();     % initialise the model
+ikTool.setModel(model);
+
+% search for the name of the output file
+[~, name, ~]=fileparts(output_IK);
+
+% set the events
+ikTool.setStartTime(event(1));
+ikTool.setEndTime(event(2));
+
+% Setup the ikTool for this trial
+ikTool.setName(name);
+
+% set up the motion file
+ikTool.setMarkerDataFileName(input_file);
+
+%set up the output file
+ikTool.setOutputMotionFileName(output_IK);
+
+% Save the settings in a setup file
+ikTool.print(output_settings);
+
+% Run IK
+ikTool.run();
+
+if BoolDiary
+    diary off
+end
+
+end
+
---
+++ ReadWrite/importTRCdata.m
@@ -0,0 +1,135 @@
+function [TRCdata, labels] = importTRCdata(filename, startRow, endRow)
+% --------------------------------------------------------------------------
+%importTRCdata 
+%   Reads data from rows startRow through endRow of text file filename.
+% 
+% INPUT:
+%   filename
+%       .trc file name from which the data should be read
+%  
+%   startRow
+%   	row to start reading
+%   
+%   endRow
+%       row to end reading
+% 
+% OUTPUT:
+%   TRCdata
+%   	imported data
+%
+%   labels
+%       data labels
+% 
+% Original author: MATLAB
+% Original date: 2014/03/04
+% --------------------------------------------------------------------------
+
+nr_markers = import_number_markers(filename);
+nr_markers = nr_markers{1};
+
+%% Initialize variables.
+delimiter = '\t';
+if nargin<=2
+    startRow = 4;
+    endRow = 4;
+end
+
+%% Format string for each line of text:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%s%s';
+
+for i=1:1:nr_markers
+    formatSpec = [formatSpec, '%s%s%s'];
+end
+formatSpec = [formatSpec, '%[^\n\r]'];
+
+% formatSpec = '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%[^\n\r]';
+
+%% Open the text sile.
+fileID = fopen(filename,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+textscan(fileID, '%[^\n\r]', startRow(1)-1, 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'ReturnOnError', false);
+for block=2:length(startRow)
+    frewind(fileID);
+    textscan(fileID, '%[^\n\r]', startRow(block)-1, 'ReturnOnError', false);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'ReturnOnError', false);
+    for col=1:length(dataArray)
+        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
+    end
+end
+
+%% Close the text file.
+fclose(fileID);
+
+%% Post processing for unimportable data.
+% No unimportable data rules were applied during the import, so no post
+% processing code is included. To generate code which works for
+% unimportable data, select unimportable cells in a file and regenerate the
+% script.
+
+%% Create output variable
+headers = [dataArray{1:end-1}];
+
+label_index = 1;
+for i=1:1:length(headers)
+    if strcmp(headers{i}, '' )==0
+        labels{label_index} = headers{i};
+        label_index = label_index+1;
+    end
+end
+
+% nr_markers = length(labels)-2;
+
+%% Initialize variables.
+delimiter = '\t';
+if nargin<=2
+    startRow = 6;
+    endRow = inf;
+end
+
+%% Format string for each line of text:
+% For more information, see the TEXTSCAN documentation.
+
+formatSpec = '%f%f';
+
+for i=1:1:nr_markers
+    formatSpec = [formatSpec, '%f%f%f'];
+%     formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+end
+formatSpec = [formatSpec, '%[^\n\r]'];
+
+%% Open the text file.
+fileID = fopen(filename,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+textscan(fileID, '%[^\n\r]', startRow(1)-1, 'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'ReturnOnError', false);
+for block=2:length(startRow)
+    frewind(fileID);
+    textscan(fileID, '%[^\n\r]', startRow(block)-1, 'ReturnOnError', false);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'ReturnOnError', false);
+    for col=1:length(dataArray)
+        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
+    end
+end
+
+%% Close the text file.
+fclose(fileID);
+
+%% Post processing for unimportable data.
+% No unimportable data rules were applied during the import, so no post
+% processing code is included. To generate code which works for
+% unimportable data, select unimportable cells in a file and regenerate the
+% script.
+
+%% Create output variable
+TRCdata = [dataArray{1:end-1}];
+
