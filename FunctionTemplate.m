f66d07dcb8c4ebe8c5bdf9de70b4a66385000e3e
Maarten Afschrift <maarten.afschrift@kuleuven.be>
2022-05-05 12:39:08 +0200

    added function template again

---
+++ FunctionTemplate.m
@@ -0,0 +1,24 @@
+function [outputArg1,outputArg2] = FunctionName(inputArg1,inputArg2)
+% --------------------------------------------------------------------------
+%FUNCTIONNAME 
+%   Detailed explanation goes here
+% 
+% INPUT:
+%   -inputArg1-
+%   * description of this input
+%  
+%   -inputArg2-
+%   * description of this input
+% 
+% OUTPUT:
+%   -outputArg1-
+%   * description of this output
+%
+%   -outputArg2-
+%   * description of this output
+% --------------------------------------------------------------------------
+
+
+
+
+end
